aaecbb92a883b818d8f430b792969309f56ea03d
Gheorghe Tarcea <tarcea@gmail.com>
2020-08-11 09:51:12 +0200

    routing done

---
+++ src/components/App/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+//PAGES
+import Navigation from '../Navigation';
+import Landing from '../Landing';
+import SignUp from '../SignUp';
+import SignIn from '../SignIn';
+import PasswordForget from '../PasswordForget';
+import Home from '../Home';
+import Account from '../Account';
+import Admin from '../Admin';
+
+import * as ROUTES from '../../constants/routes';
+
+const App = () => (
+    <Router>
+      <div>
+        <Navigation />
+        <hr />
+
+        <Route exact path={ROUTES.LANDING} component={Landing} />
+        <Route path={ROUTES.SIGN_UP} component={SignUp} />
+        <Route path={ROUTES.SIGN_IN} component={SignIn} />
+        <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForget} />
+        <Route path={ROUTES.HOME} component={Home} />
+        <Route path={ROUTES.ACCOUNT} component={Account} />
+        <Route path={ROUTES.ADMIN} component={Admin} />
+      </div>
+    </Router>
+);
+
+export default App;
